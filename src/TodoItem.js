ebfba694d86ef0101016c3e49914d133a9a48bfd
Daniel Floyd <gleek.dan@gmail.com>
2020-06-10 12:00:24 -0600

    added ability to toggle a todo done

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,13 +3,40 @@ import React from 'react';
 class TodoItem extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            done: props.todoData.done
+        }
+    }
+
+    toggleDone = () => {
+        fetch(`http://localhost:5000/todo/${this.props.todoData.id}`, {
+            method: "PATCH",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify({
+                done: !this.state.done
+            })
+        })
+        .then(() => {
+            this.setState({
+                done: !this.state.done
+            })
+        })
+        .catch(err => {
+            console.log("toggleDone Error: ", err)
+        })
     }
 
     render() {
         return (
             <div className="todo-item">
-                <input type="checkbox" />
-                <p>{this.props.todoData.title}</p>
+                <input
+                    type="checkbox"
+                    defaultChecked={this.state.done}
+                    onClick={this.toggleDone}
+                />
+                <p className={this.state.done ? "done" : null}>
+                    {this.props.todoData.title}
+                </p>
             </div>
         )
     }
