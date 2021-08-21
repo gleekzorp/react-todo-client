e4a256a757711025ccfc201e1b5017c845a54009
Daniel <gleek.dan@gmail.com>
2021-08-21 17:55:23 -0600

    Formatting and update readme

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,52 +1,58 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 class TodoItem extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            done: props.todoData.done
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: props.todoData.done,
+    };
+  }
 
-    toggleDone = () => {
-        // fetch(`http://localhost:5000/todo/${this.props.todoData.id}`, {
-        fetch(`https://df-flask-todo-api-class.herokuapp.com/todo/${this.props.todoData.id}`, {
-            method: "PATCH",
-            headers: { "content-type": "application/json" },
-            body: JSON.stringify({
-                done: !this.props.todoData.done
-            })
-        })
-        .then(() => {
-            this.props.handleSuccessfulDone(this.props.todoData.id)
-        })
-        .catch(err => {
-            console.log("toggleDone Error: ", err)
-        })
-    }
+  toggleDone = () => {
+    // fetch(`http://localhost:5000/todo/${this.props.todoData.id}`, {
+    fetch(
+      `https://df-flask-todo-api-class.herokuapp.com/todo/${this.props.todoData.id}`,
+      {
+        method: "PATCH",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({
+          done: !this.props.todoData.done,
+        }),
+      }
+    )
+      .then(() => {
+        this.props.handleSuccessfulDone(this.props.todoData.id);
+      })
+      .catch((err) => {
+        console.log("toggleDone Error: ", err);
+      });
+  };
 
-    render() {
-        return (
-            <div className="todo-item">
-                <div className="todo-item-left">
-                    <input
-                        type="checkbox"
-                        defaultChecked={this.state.done}
-                        onClick={this.toggleDone}
-                        className="checkbox"
-                    />
-                    <p className={this.props.todoData.done ? "done" : null}>
-                        {this.props.todoData.title}
-                    </p>
-                </div>
-                <div onClick={() => this.props.deleteTodo(this.props.todoData.id)} className="delete-btn">
-                    <FontAwesomeIcon icon={faTrash} />
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="todo-item">
+        <div className="todo-item-left">
+          <input
+            type="checkbox"
+            defaultChecked={this.state.done}
+            onClick={this.toggleDone}
+            className="checkbox"
+          />
+          <p className={this.props.todoData.done ? "done" : null}>
+            {this.props.todoData.title}
+          </p>
+        </div>
+        <div
+          onClick={() => this.props.deleteTodo(this.props.todoData.id)}
+          className="delete-btn"
+        >
+          <FontAwesomeIcon icon={faTrash} />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default TodoItem
+export default TodoItem;
--- src/index.js
+++ src/index.js
@@ -1,39 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import axios from 'axios';
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
 
-import './style.css'
-import TodoItem from './TodoItem';
+import "./style.css";
+import TodoItem from "./TodoItem";
 
 class App extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
       todo: "",
-      todos: []
-    }
+      todos: [],
+    };
   }
 
   handleChange = (e) => {
     this.setState({
-      todo: e.target.value
-    })
-  }
+      todo: e.target.value,
+    });
+  };
 
   handleSuccessfulDone = (id) => {
-    let todos = this.state.todos
-    todos.forEach(todo => {
+    let todos = this.state.todos;
+    todos.forEach((todo) => {
       if (todo.id === id) {
-        todo.done = !todo.done
+        todo.done = !todo.done;
       }
-    })
+    });
     this.setState({
-      todos: todos
-    })
-  }
+      todos: todos,
+    });
+  };
 
   addTodo = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     axios({
       method: "post",
       // url: "http://localhost:5000/todo",
@@ -41,76 +41,78 @@ class App extends React.Component {
       headers: { "content-type": "application/json" },
       data: {
         title: this.state.todo,
-        done: false
-      }
+        done: false,
+      },
     })
-    .then(res => {
-      this.setState({
-        todos: [...this.state.todos, res.data],
-        todo: ""
+      .then((res) => {
+        this.setState({
+          todos: [...this.state.todos, res.data],
+          todo: "",
+        });
       })
-    })
-    .catch(err => {
-      console.log("addTodo Error: ", err)
-    })
-  }
+      .catch((err) => {
+        console.log("addTodo Error: ", err);
+      });
+  };
 
   deleteTodo = (id) => {
     // fetch(`http://localhost:5000/todo/${id}`, {
     fetch(`https://df-flask-todo-api-class.herokuapp.com/todo/${id}`, {
-      method: 'DELETE'
+      method: "DELETE",
     })
-    .then(() => {
-      this.setState({
-        todos: this.state.todos.filter(todo => {
-          return todo.id !== id
-        })
-      })
-    })
-    .catch(err => {
-      console.log('DeleteItem Error: ', err)
-    })
-  }
-
-  deleteFinishedTodos = () => {
-    let finished_todos = this.state.todos.filter(todo => {
-      return todo.done === true
-    })
-    axios.all(finished_todos.map((finished_todo) => {
-      axios({
-        // url: `http://localhost:5000/todo/${finished_todo.id}`,
-        url: `https://df-flask-todo-api-class.herokuapp.com/todo/${finished_todo.id}`,
-        method: "delete"
-      })
       .then(() => {
-        this.setState((prevState) => ({
-          todos: prevState.todos.filter(todo => {
-            return todo.id !== finished_todo.id
-          })
-        }))
+        this.setState({
+          todos: this.state.todos.filter((todo) => {
+            return todo.id !== id;
+          }),
+        });
       })
       .catch((err) => {
-        console.log('deleteFinishedTodos Error', err)
+        console.log("DeleteItem Error: ", err);
+      });
+  };
+
+  deleteFinishedTodos = () => {
+    let finished_todos = this.state.todos.filter((todo) => {
+      return todo.done === true;
+    });
+    axios.all(
+      finished_todos.map((finished_todo) => {
+        axios({
+          // url: `http://localhost:5000/todo/${finished_todo.id}`,
+          url: `https://df-flask-todo-api-class.herokuapp.com/todo/${finished_todo.id}`,
+          method: "delete",
+        })
+          .then(() => {
+            this.setState((prevState) => ({
+              todos: prevState.todos.filter((todo) => {
+                return todo.id !== finished_todo.id;
+              }),
+            }));
+          })
+          .catch((err) => {
+            console.log("deleteFinishedTodos Error", err);
+          });
       })
-    }))
-  }
+    );
+  };
 
   componentDidMount() {
     // fetch("http://localhost:5000/todos")
     fetch("https://df-flask-todo-api-class.herokuapp.com/todos")
-      .then(res => res.json())
-      .then(data => {
+      .then((res) => res.json())
+      .then((data) => {
         this.setState({
-          todos: data
-        })
+          todos: data,
+        });
       })
-      .catch(err => {
-        console.log('FetchTodos Error: ', err)
-      })
+      .catch((err) => {
+        console.log("FetchTodos Error: ", err);
+      });
   }
 
   renderTodos = () => {
-    return this.state.todos.map(todo => {
+    return this.state.todos.map((todo) => {
       return (
         <TodoItem
           key={todo.id}
@@ -118,9 +120,9 @@ class App extends React.Component {
           deleteTodo={this.deleteTodo}
           handleSuccessfulDone={this.handleSuccessfulDone}
         />
-      )
-    })
-  }
+      );
+    });
+  };
 
   render() {
     return (
@@ -137,11 +139,13 @@ class App extends React.Component {
         </form>
         {this.renderTodos()}
         {this.state.todos.length > 1 ? (
-          <button className="delete-all-btn" onClick={this.deleteFinishedTodos}>Delete Finished Todos</button>
+          <button className="delete-all-btn" onClick={this.deleteFinishedTodos}>
+            Delete Finished Todos
+          </button>
         ) : null}
       </div>
-    )
+    );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
